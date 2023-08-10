d8fe225dcadad6669d5d064903f7cc40709f4196
Rae Gebhart <raegebhart@gmail.com>
2023-08-10 12:34:19 -0600

    Format authors name

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,11 +1,13 @@
 import "./Product.css";
 
 export const Product = ({ image, author, name }) => {
+  let formatAuthor = author?.split(',')
   return (
     <div style={{ backgroundImage: `url(${image})` }} className="Product">
       <div className="catalog-info">
         <p>{name}</p>
-        <p>{author}</p>
+        {author && <p>by</p>}
+        {author && <p>{formatAuthor[1]} {formatAuthor[0]}</p>}
       </div>
     </div>
   );
